b481f0928685143a9ef8d6b8067bce7413ce4a6a
Geetika Singh <geetika.singh@duke.edu>
2021-06-11 12:28:21 -0400

    Remove other folders

--- Signal-Alignment/debug/dbd_cf.py
+++
@@ -1,8 +0,0 @@
-debug_file = 'data/Beef_TRAIN'
-debug_line = 1
-
-font_xlabel = {
-    'family': 'Arial',
-    'weight': 'normal',
-    'size': 30
-}
--- Signal-Alignment/sdtw/config.py
+++
@@ -1,4 +0,0 @@
-nBlocks = 2
-nbins = 8
-dx_scale = 0.1
-sub_len = 19
